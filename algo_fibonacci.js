01bc742ed2c94ada3276e6590da56520b4407e4b
Fernanda Monroe <141519747+NandaMonroe@users.noreply.github.com>
2023-09-25 19:03:52 -0300

    Initial commit
    Find the number of the given index of the fibonacci sequence

---
+++ algo_fibonacci.js
@@ -0,0 +1,80 @@
+/* 
+  Return the fibonacci number at the nth position, recursively.
+  
+  Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+
+  The next number is found by adding up the two numbers before it,
+  starting with 0 and 1 as the first two numbers of the sequence.
+*/
+
+const num1 = 0;
+const expected1 = 0;
+
+const num2 = 1;
+const expected2 = 1;
+
+const num3 = 2;
+const expected3 = 1;
+
+const num4 = 3;
+const expected4 = 2;
+
+const num5 = 4;
+const expected5 = 3;
+
+const num6 = 8;
+const expected6 = 21;
+
+/**
+ * Recursively finds the nth number in the fibonacci sequence.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {number} num The position of the desired number in the fibonacci sequence.
+ * @returns {number} The fibonacci number at the given position.
+ */
+
+// USING RECURSION FUNCTION
+function fibonacci(num) {
+
+    if(num == 0) 
+    return 0; 
+
+    if (num == 1) 
+    return 1; 
+
+    return  fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+console.log(fibonacci(num1));
+console.log(fibonacci(num2));
+console.log(fibonacci(num3));
+console.log(fibonacci(num4));
+console.log(fibonacci(num5));
+console.log(fibonacci(num6));
+
+/*****************************************************************************/
+
+// USING FOR LOOP
+
+function fibonacci(num) {
+
+    var num1 = 0; 
+    var num2 = 1; 
+    var sum = 0 
+     
+    for (var i = 0; i < num; i++) {
+
+        sum = num1 + num2; 
+        num1 = num2; 
+        num2 = sum; 
+    }
+
+    return num2; 
+}
+
+console.log(fibonacci(num1));
+console.log(fibonacci(num2));
+console.log(fibonacci(num3));
+console.log(fibonacci(num4));
+console.log(fibonacci(num5));
+console.log(fibonacci(num6));
