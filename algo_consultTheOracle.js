1ace3e404ac8cfe549bfac32d5ba8ba5ef8ee694
Fernanda Monroe <141519747+NandaMonroe@users.noreply.github.com>
2024-03-27 20:52:43 -0300

    Add files via upload

---
+++ algo_consultTheOracle.js
@@ -0,0 +1,42 @@
+/* 
+  Consult the Oracle
+
+  Given an array of wise answers, randomly choose a response and return it
+*/
+
+var lifesAnswers = [
+  "It is certain.",
+  "It is decidedly so.",
+  "Without a doubt.",
+  "Yes – definitely.",
+  "You may rely on it.",
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes.",
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
+  "Don't count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful."
+];
+
+/**
+* Given an array of wise answers, randomly choose a response and return it
+* @param {Array<any>} arr
+* @returns {string} the randomly chosen oracle response 
+*/
+
+function consultTheOracle(arr) {
+
+    var item = arr[Math.ceil(Math.random()*arr.length)];
+    return item;
+}
+
+console.log(consultTheOracle(lifesAnswers));
